7e3a2098c92520c93e926d653f6bd68dc4c02410
김진호 <rlawlsgh1227@gmail.com>
2023-07-11 15:07:22 +0900

    design: 로그인 회원가입 메인페이지 프레임 구성 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
+import theme from '../styles/Theme'
 
 function Header() {
+  const navigate = useNavigate()
+  const goLogin = () => {
+    navigate('/login')
+  }
   return (
     <HeaderContainer>
-      <Link to="/">
-        <Logo src="/images/logo.png" alt="로고" />
-      </Link>
+      <Logo to="/" />
+      <Navigation>
+        <NavigationItem to="/">지원하기</NavigationItem>
+        <NavigationItem to="/community">커뮤니티</NavigationItem>
+        <UserButton onClick={goLogin}>
+          <UserIcon src="/images/userIcon.png" alt="아이콘" />
+          <UserButtonText>로그인</UserButtonText>
+        </UserButton>
+      </Navigation>
     </HeaderContainer>
   )
 }
@@ -17,9 +28,52 @@ export default Header
 const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-around;
+  align-items: center;
   width: 100%;
-  height: 60px;
-  background-color: black;
+  height: 80px;
+  background-color: ${theme.colors.black};
 `
 
-const Logo = styled.img``
+const Logo = styled(Link)`
+  width: 190px;
+  height: 22px;
+  background-image: url('/images/logo.png');
+`
+
+const Navigation = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 30%;
+`
+
+const NavigationItem = styled(Link)`
+  color: ${theme.colors.white};
+  font-size: ${theme.fontSizes.navigation};
+  text-decoration: none;
+`
+
+const UserButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 130px;
+  height: 40px;
+  padding: 8px 12px;
+  background-color: ${theme.colors.gray};
+  border: none;
+  border-radius: 20px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const UserIcon = styled.img`
+  margin-right: 10px;
+`
+
+const UserButtonText = styled.span`
+  color: ${theme.colors.white};
+  font-size: ${theme.fontSizes.navigation};
+`
